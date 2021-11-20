c110d13ed6d04a55f3015001ea7f8a60ac93749d
unknown <tomibami2020@gmail.com>
2021-11-20 23:52:29 +0100

    added example 03

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
+// wg is the pointer to a waitgroup
 // name is an string to identify the function call
 // limit the amount of number the funcion will print
 // sleep is the amount of seconds before the function prints the next value
-func randSleep(name string, limit int, sleep int) {
+func randSleep(wg *sync.WaitGroup, name string, limit int, sleep int) {
+	defer wg.Done()
 	for i := 1; i <= limit; i++ {
 		fmt.Println(name, rand.Intn(i))
 		time.Sleep(time.Duration(sleep * int(time.Second)))
@@ -18,16 +21,28 @@ func randSleep(name string, limit int, sleep int) {
 
 }
 func main() {
-	go randSleep("first:", 10, 2)
-	randSleep("second:", 3, 2)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	go randSleep(wg, "first:", 10, 2)
+	go randSleep(wg, "second:", 3, 2)
+	wg.Wait()
 
 }
 
+// OUTPUT
+
 // second: 0
 // first: 0
+// first: 1
 // second: 1
+// second: 1
+// first: 0
 // first: 1
+// first: 0
+// first: 4
 // first: 1
-// second: 0
+// first: 6
+// first: 7
+// first: 2
 
-// git checkout 02
+// git checkout 03
