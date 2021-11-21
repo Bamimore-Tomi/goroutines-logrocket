49419040bc93c08cb24788f007c22516d5358905
unknown <tomibami2020@gmail.com>
2021-11-21 11:58:57 +0100

    added example 5

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func main() {
-	// creating a channel by declaring it
-	var mychannel1 chan int
-	fmt.Println(mychannel1)
-
-	// creating a channel using make()
-
-	mychannel2 := make(chan int)
-	fmt.Println(mychannel2)
+func writeChannel(wg *sync.WaitGroup, limitchannel chan int, stop int) {
+	defer wg.Done()
+	for i := 1; i <= stop; i++ {
+		limitchannel <- i
+	}
 
 }
+
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	limitchannel := make(chan int, 2)
+	defer close(limitchannel)
+	go writeChannel(wg, limitchannel, 2)
+	wg.Wait()
+	fmt.Println(<-limitchannel)
+	fmt.Println(<-limitchannel)
+
+}
+
+// OUTPUT
+
+// 1
+// 2
+
+// git checkout 05
